5a69186ca84c11ab9b0c19833e307632e2457db8
Daniel Ortega <danielortegaberja@gmail.com>
2025-05-31 23:08:58 +0200

    feat: flow deployment.

--- backend/README.md
+++ backend/README.md
@@ -247,8 +247,8 @@ npx hardhat liquidity:add \
   --contract 0x5FbDB2315678afecb367f032d93F642f64180aa3 \
   --company 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 \
   --pool 0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9 \
-  --amount0 10.0 \
-  --amount1 10.0 \
+  --amount0 0.5 \
+  --amount1 0.5 \
   --network localhost
 ```
 
--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -23,31 +23,33 @@ const config: HardhatUserConfig = {
     hardhat: {
       chainId: 31337,
       forking: {
-        url: process.env.FLOW_RPC_URL || "",
+        url: process.env.FLOW_EVM_RPC_URL || "",
         blockNumber: process.env.BLOCK_NUMBER ? parseInt(process.env.BLOCK_NUMBER) : undefined
       }
+    },
+    flow_evm: {
+      url: process.env.FLOW_EVM_RPC_URL || "https://mainnet.evm.nodes.onflow.org",
+      accounts: [process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY : ""],
+      chainId: 747,
+      gasPrice: 50000000000, // 50 gwei
+      gasMultiplier: 1.5
     },
     sepolia: {
       url: process.env.SEPOLIA_RPC_URL || "",
       accounts: [process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY : ""],
       chainId: 11155111
     },
-    flow: {
-      url: process.env.FLOW_RPC_URL || "",
-      accounts: [process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY : ""],
-      chainId: 1337
-    },
     optimism_testnet: {
       eid: EndpointId.OPTSEP_V2_TESTNET,
       url: process.env.RPC_URL_OP_SEPOLIA || 'https://optimism-sepolia.gateway.tenderly.co',
       accounts: [process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY : ""],
-  },
-  avalanche_testnet: {
+    },
+    avalanche_testnet: {
       eid: EndpointId.AVALANCHE_V2_TESTNET,
       url: process.env.RPC_URL_FUJI || 'https://avalanche-fuji.drpc.org',
       accounts: [process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY : ""],
-  },
-  arbitrum_testnet: {
+    },
+    arbitrum_testnet: {
       eid: EndpointId.ARBSEP_V2_TESTNET,
       url: process.env.RPC_URL_ARB_SEPOLIA || 'https://arbitrum-sepolia.gateway.tenderly.co',
       accounts: [process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY : ""],
