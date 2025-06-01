e2162d12a7dfd1f97eb6aab3098a6f276a5154db
Miquel Suau Gonzàlez <sg.miquel@gmail.com>
2025-06-01 06:04:38 +0200

    Update api_calls.ts

--- frontend/src/api/api_calls.ts
+++ frontend/src/api/api_calls.ts
@@ -327,7 +327,7 @@ export const fetchIndividualBalance = async (
     const tokenContract = getContract({
       address: tokenAddress as `0x${string}`,
       abi: erc20Abi,
-      publicClient
+      client: publicClient
     });
 
     // Get token info and balance
